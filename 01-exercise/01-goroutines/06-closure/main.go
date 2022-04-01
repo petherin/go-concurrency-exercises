f39439f8a22957cae497f9610c3fdb18eff457c9
Peter Etherington <Peter.Etherington@skybettingandgaming.com>
2022-04-01 15:11:15 +0100

    01-01-06 pass variable to goroutine so it operates on the current value

--- 01-exercise/01-goroutines/06-closure/main.go
+++ 01-exercise/01-goroutines/06-closure/main.go
@@ -8,15 +8,32 @@ import (
 func main() {
 	var wg sync.WaitGroup
 
-	// what is the output
-	//TODO: fix the issue.
+	// What is the output?
+	// Before fix, this prints out 4 three times.
+	// This is because the loop increments i to 4
+	// before any of the goroutines get a chance to run.
+	// By the time they are running, i is already at 4.
+	// Goroutines operate on the current value of their
+	// variables at the time of their execution.
 
+	// Broken code
+	//for i := 1; i <= 3; i++ {
+	//	wg.Add(1)
+	//	go func() {
+	//		defer wg.Done()
+	//		fmt.Println(i)
+	//	}()
+	//}
+
+	// Fixed code
+	// If we want the goroutine to operate on a specific
+	// value, we have to pass it to the goroutine.
 	for i := 1; i <= 3; i++ {
 		wg.Add(1)
-		go func() {
+		go func(i int) {
 			defer wg.Done()
 			fmt.Println(i)
-		}()
+		}(i)
 	}
 	wg.Wait()
 }
