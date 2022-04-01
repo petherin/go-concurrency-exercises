49dc2c5a2eed96247a78136bce78cb7ced9c135b
Peter Etherington <Peter.Etherington@skybettingandgaming.com>
2022-04-01 14:33:30 +0100

    01-01-02 server with multiple concurrent client connections

--- 01-exercise/01-goroutines/02-client-server/server/main.go
+++ 01-exercise/01-goroutines/02-client-server/server/main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"io"
+	"log"
 	"net"
 	"time"
 )
 
 func main() {
-	// TODO: write server program to handle concurrent client connections.
+	// write server program to handle concurrent client connections.
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+
+		// Adding go here means we can handle multiple concurrent client connections
+		go handleConn(conn)
+	}
 }
 
 // handleConn - utility function
