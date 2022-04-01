dba9ef06ecf604a01481b7b771258f3e0795a56a
Peter Etherington <Peter.Etherington@skybettingandgaming.com>
2022-04-01 14:13:49 +0100

    01-01-01 Basic starting of goroutines

--- 01-exercise/01-goroutines/01-hello/main.go
+++ 01-exercise/01-goroutines/01-hello/main.go
@@ -19,12 +19,23 @@ func main() {
 	// TODO: write goroutine with different variants for function call.
 
 	// goroutine function call
+	// This won't have time to run before main() completes.
+	// So add a time.Sleep to block for 100 ms so that the goroutines
+	// have time to finish.
+	go fun("goroutine-1")
 
 	// goroutine with anonymous function
+	go func() {
+		fun("goroutine-2")
+	}()
 
 	// goroutine with function value call
+	fv := fun
+	go fv("goroutine-3")
 
 	// wait for goroutines to end
+	fmt.Println("wait for goroutines")
+	time.Sleep(100 * time.Millisecond)
 
-	fmt.Println("done..")
+	fmt.Println("done")
 }
