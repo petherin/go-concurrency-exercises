9cf6c4c1c2cd2eab6446b7ee2bc3c462ad16c889
Peter Etherington <Peter.Etherington@skybettingandgaming.com>
2022-04-01 17:51:36 +0100

    01-03-02 time.After in a select for a timeout

--- 01-exercise/03-select/02-select/main.go
+++ 01-exercise/03-select/02-select/main.go
@@ -13,8 +13,11 @@ func main() {
 		ch <- "one"
 	}()
 
-	// TODO: implement timeout for recv on channel ch
-
-	m := <-ch
-	fmt.Println(m)
+	// implement timeout for recv on channel ch
+	select {
+	case m := <-ch:
+		fmt.Println(m)
+	case <-time.After(1 * time.Second):
+		fmt.Println("timeout")
+	}
 }
