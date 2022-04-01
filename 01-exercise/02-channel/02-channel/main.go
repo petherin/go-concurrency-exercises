19fa7ac6e5a79b9cc6590cf54f3c90e6b9b700c8
Peter Etherington <Peter.Etherington@skybettingandgaming.com>
2022-04-01 16:07:17 +0100

    01-02-02 Iterate over unbuffered channel

--- 01-exercise/02-channel/02-channel/main.go
+++ 01-exercise/02-channel/02-channel/main.go
@@ -1,12 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
+	ch := make(chan int)
+
 	go func() {
 		for i := 0; i < 6; i++ {
-			// TODO: send iterator over channel
+			// send iterator over channel
+			ch <- i
 		}
+
+		// breaks out the loop below that's ranging over the channel
+		close(ch)
 	}()
 
-	// TODO: range over channel to recv values
-
+	// range over channel to recv values
+	for v := range ch {
+		fmt.Println(v)
+	}
 }
