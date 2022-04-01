f5bcb00fc10951ff5a1f44d3d52cb06b3e675474
Peter Etherington <Peter.Etherington@skybettingandgaming.com>
2022-04-01 19:31:30 +0100

    01-03-03 default case in select makes it non-blocking

--- 01-exercise/03-select/03-select/main.go
+++ 01-exercise/03-select/03-select/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 )
 
@@ -11,18 +12,40 @@ func main() {
 	go func() {
 		for i := 0; i < 3; i++ {
 			time.Sleep(1 * time.Second)
-			ch <- "message"
+			msg := fmt.Sprintf("message%d", i)
+			log.Printf("goroutine: sending %s on channel", msg)
+			ch <- msg
 		}
-
 	}()
 
-	// TODO: if there is no value on channel, do not block.
-	for i := 0; i < 2; i++ {
-		m := <-ch
-		fmt.Println(m)
+	// if there is no value on channel, do not block.
+	// Before fix
+	//for i := 0; i < 2; i++ {
+	// This will block until there's a message, blocking
+	// execution of the work below.
+	//	m := <-ch
+	//	log.Printf("main routine: %s received", m)
+	//
+	//	// Do some processing
+	//	log.Println("main routine: processing for 1500 ms")
+	//	time.Sleep(1500 * time.Millisecond)
+	//}
 
-		// Do some processing..
-		fmt.Println("processing..")
+	// After fix
+	for i := 0; i < 2; i++ {
+		// In this select, if there is no message then the default
+		// case is hit, and the select does not block, so we can
+		// proceed onto some other work. When the work is finished
+		// the loop sends us back to the select.
+		select {
+		case m := <-ch:
+			log.Printf("main routine: %s received", m)
+		default:
+			log.Println("main routine: no message received")
+		}
+
+		// Do some processing
+		log.Println("main routine: processing for 1500 ms")
 		time.Sleep(1500 * time.Millisecond)
 	}
 }
