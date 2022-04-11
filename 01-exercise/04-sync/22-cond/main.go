342da5580d08d84da110ba044cfd2b712e7ef293
Peter Etherington <Peter.Etherington@skybettingandgaming.com>
2022-04-11 17:50:53 +0100

    01-04-22 sync.Broadcast to wake up all Waiting goroutines

--- 01-exercise/04-sync/22-cond/main.go
+++ 01-exercise/04-sync/22-cond/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"sync"
 	"time"
 )
@@ -11,35 +12,51 @@ var sharedRsc = make(map[string]interface{})
 func main() {
 	var wg sync.WaitGroup
 
+	mu := sync.Mutex{}
+	c := sync.NewCond(&mu)
+
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 
-		//TODO: suspend goroutine until sharedRsc is populated.
+		// suspend goroutine until sharedRsc is populated.
 
-		for len(sharedRsc) == 0 {
-			time.Sleep(1 * time.Millisecond)
+		log.Println("goroutine1: about to wait")
+		c.L.Lock()
+		for len(sharedRsc) < 1 {
+			//time.Sleep(1 * time.Millisecond)
+			c.Wait()
 		}
 
-		fmt.Println(sharedRsc["rsc1"])
+		fmt.Printf("goroutine1: %s\n", sharedRsc["rsc1"])
+		c.L.Unlock()
 	}()
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 
-		//TODO: suspend goroutine until sharedRsc is populated.
+		// suspend goroutine until sharedRsc is populated.
 
-		for len(sharedRsc) == 0 {
-			time.Sleep(1 * time.Millisecond)
+		log.Println("goroutine2: about to wait")
+		c.L.Lock()
+		for len(sharedRsc) < 2 {
+			//time.Sleep(1 * time.Millisecond)
+			c.Wait()
 		}
 
-		fmt.Println(sharedRsc["rsc2"])
+		fmt.Printf("goroutine2: %s\n", sharedRsc["rsc2"])
+		c.L.Unlock()
 	}()
 
+	c.L.Lock()
 	// writes changes to sharedRsc
+	time.Sleep(1 * time.Second)
 	sharedRsc["rsc1"] = "foo"
+	time.Sleep(1 * time.Second)
 	sharedRsc["rsc2"] = "bar"
-
+	log.Println("main routine: wake all goroutines with Broadcast")
+	c.Broadcast() // Wakes up all goroutines
+	c.L.Unlock()
 	wg.Wait()
 }
