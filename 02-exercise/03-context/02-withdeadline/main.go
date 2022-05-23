2b767de12ebcdbc7fdade13a5646a3867b1a86e8
Peter Etherington <Peter.Etherington@skybettingandgaming.com>
2022-05-23 16:19:00 +0100

    02-03-02 create context with deadline

--- 02-exercise/03-context/02-withdeadline/main.go
+++ 02-exercise/03-context/02-withdeadline/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 )
@@ -12,23 +13,41 @@ type data struct {
 func main() {
 
 	// TODO: set deadline for goroutine to return computational result.
+	deadline := time.Now().Add(10 * time.Millisecond)
+	ctx, cancel := context.WithDeadline(context.Background(), deadline)
+	defer cancel()
 
 	compute := func() <-chan data {
 		ch := make(chan data)
 		go func() {
 			defer close(ch)
+			deadline, ok := ctx.Deadline()
+			if ok {
+				if deadline.Sub(time.Now().Add(50*time.Millisecond)) < 0 {
+					fmt.Println("insufficient time... terminating")
+					return
+				}
+			}
+
 			// Simulate work.
 			time.Sleep(50 * time.Millisecond)
 
 			// Report result.
-			ch <- data{"123"}
+			select {
+			case ch <- data{"123"}:
+			case <-ctx.Done():
+				return
+			}
+
 		}()
 		return ch
 	}
 
 	// Wait for the work to finish. If it takes too long move on.
 	ch := compute()
-	d := <-ch
-	fmt.Printf("work complete: %s\n", d)
+	d, ok := <-ch
+	if ok {
+		fmt.Printf("work complete: %s\n", d)
+	}
 
 }
